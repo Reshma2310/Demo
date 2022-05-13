746f4c6e3a27a8c5fef5d05f8552068d098112c5
Reshma2310 <dreshma253@gmail.com>
2022-05-13 13:22:17 +0500

    [Reshma] - Addition of two Numbers

---
+++ Test1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Test1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Enter first number");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Addition2No addition = new Addition2No();
+            z = addition.add(x, y);
+            Console.WriteLine("Addition of two numbers is " + z);
+        }
+    }
+}
